021d744132810a18fd5653818955225e863cc8a5
Pietrodemartino914 <115526682+Pietrodemartino914@users.noreply.github.com>
2022-11-22 20:34:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 using namespace std;
 
+void fibonacci (int n ){
+    int a=1 ;
+    int b=1 ;
+    while(n>2){
+         a+=b;
+         cout << a << endl;
+         if (n==3){
+             n--;
+             break;
+         }else {
+             b +=a;
+             cout << b << endl ;
+             n--;
+             n--;
+         }
+
+    }
+
+    }
+
+
+
+
 int main() {
-   return 0;
+    int a = 0;
+    cin >> a;
+    if (a>2){
+        cout << "1" << endl;
+        cout << "1" << endl;
+
+        fibonacci(a);
+
+    } else {
+        cout << "errore" << endl;
+    }
+    return 0;
 }
